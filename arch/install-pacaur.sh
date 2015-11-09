1de2240b0a614d5feb82d7fbdafa52d2868fa18e
Carter Hall <cahruhr2795@gmail.com>
2015-11-09 06:24:25 -0500

    add pacaur installation script

---
+++ arch/install-pacaur.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Stefan Tatschner
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+buildroot=$(mktemp -d)
+
+# Ask for user passwort once, see sudo(8).
+sudo -v
+
+# Fetch Dave Reisner's key to be able to verify cower.
+gpg --recv-keys 487EACC08557AD082088DABA1EB2638FF56C0C53
+
+# Make sure we can even build packages on arch linux.
+sudo pacman -S --needed --noconfirm base-devel curl
+
+mkdir -p ${buildroot}
+mkdir ${buildroot}/cower ${buildroot}/pacaur
+
+cd ${buildroot}/cower
+curl -L https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower > PKGBUILD
+makepkg --syncdeps --install --noconfirm
+
+cd ${buildroot}/pacaur
+curl -L https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur > PKGBUILD
+makepkg --syncdeps --install --noconfirm
+
+rm -rf ${buildroot}
