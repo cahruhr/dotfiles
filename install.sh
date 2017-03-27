1066337a89eb6bc3e251781fec53b67a7c19db72
cahruhr <cahruhr2795@gmail.com>
2017-03-27 11:53:54 -0400

    update vimrc; install script includes pathogen

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ cp xorg/Xresources ~/.Xresources
 cp tmux/tmux.conf ~/.tmux.conf
 mkdir ~/.xmonad
 cp xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
